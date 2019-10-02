c948c731b2a81a08e29bead431d438b400d404d2
JackieHendrixon <holymaiden000@gmail.com>
2019-10-02 14:55:36 +0200

    handshake

--- DAT159CryptoOblig/src/no/hvl/dat159/crypto/KeyStores.java
+++ DAT159CryptoOblig/src/no/hvl/dat159/crypto/KeyStores.java
@@ -3,6 +3,12 @@
  */
 package no.hvl.dat159.crypto;
 
+import javax.net.ssl.KeyManagerFactory;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
 import java.security.PrivateKey;
 
 
@@ -15,17 +21,30 @@ public class KeyStores {
 	
 	/**
 	 * 
-	 * @param keystore
+	 * @param keyStoreFile
 	 * @param alias
-	 * @param keystorepassword
+	 * @param keyStorePassword
 	 * @return
 	 */
-	public static PrivateKey getPrivateKeyFromKeyStore(String keystore, String alias, String keystorepassword) {
+	public static PrivateKey getPrivateKeyFromKeyStore(String keyStoreFile, String alias, String keyStorePassword)
+			throws GeneralSecurityException, IOException {
 		
 		// Load the keystore (programmatically) and extract the private key from the keystore
+
+		KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+		try(InputStream in = new FileInputStream(keyStoreFile)){
+			keyStore.load(in, keyStorePassword.toCharArray());
+		}
+		catch(Exception e)
+		{
+
+		}
+		KeyManagerFactory keyManagerFactory =
+				KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		keyManagerFactory.init(keyStore, keyStorePassword.toCharArray());
+
+		PrivateKey key = (PrivateKey) keyStore.getKey(alias,keyStorePassword.toCharArray());
 		
-		// implement me
-		
-		return null;
+		return key;
 	}
 }
